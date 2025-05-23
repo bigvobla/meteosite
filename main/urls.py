fff5f10a2f93f44bb2c780b1a688dc15ae19f2fd
Sergey <simplecovachcsgo@gmail.com>
2025-05-23 19:55:52 +0500

    second

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('',views.home),
+    path('',views.home, name='home'),
     path('about/',views.about),
     path('meteodata/',views.meteodata),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
